f2c6a38b3ecb635820f8db85b0dd4e3d4861ffe4
Riley <cohriley@amazon.com>
2021-02-18 19:17:15 -0700

    Fix aws-exports import

--- App.tsx
+++ App.tsx
@@ -7,7 +7,8 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 import Amplify from 'aws-amplify'
-import config from './aws-exports'
+// @ts-ignore
+import config from './src/aws-exports'
 Amplify.configure(config)
 
 export default function App() {
