bf13dd907a2b3c1479dbf2baa9a217672c81fba6
Alex Barraza <meeatingchips@Alexs-MacBook-Pro.local>
2021-03-02 21:57:55 -0700

    Added my name, Alex Barraza to TabOneScreen.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -11,6 +11,7 @@ export default function TabOneScreen() {
           <Text>Riley</Text>
           <Text>Ryan G</Text>
           <Text>Riley 2</Text>
+          <Text>Alex Barraza</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
