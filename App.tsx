f13fae10d92d4a9f0f6006a952ac76922b6a8001
Alex Barraza <meeatingchips@Alexs-MacBook-Pro.local>
2021-03-11 17:48:58 -0700

    Worked on the HomePage setup

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+//import { withAuthenticator } from 'aws-amplify-react-native'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,6 +9,10 @@ import Navigation from './navigation';
 
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
+
+// Automatic create/sign-in account
+//export default withAuthenticator(App);
+
 Amplify.configure(config)
 
 export default function App() {
--- types.tsx
+++ types.tsx
@@ -4,8 +4,15 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;            // TabOneScreen
+  Search: undefined;          // TabTwoScreen
+  Notifications: undefined;   // TabThreeScreen
+  Messages: undefined;        // TabFourScreen
+};
+
+// Needs to be moved and renamed as TabThreeParamList
+export type HomeScreenParamList = {
+  HomeScreen: undefined;
 };
 
 export type TabOneParamList = {
@@ -15,3 +22,25 @@ export type TabOneParamList = {
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
 };
+
+export type TabFourParamList = {
+  TabFourScreen: undefined;
+};
+
+export type UserType = {
+  id: string,
+  name: string,
+  username: string,
+  image?: string
+}
+
+export type TweetType = {
+  id: string,
+  createdAt: string,
+  user: UserType,
+  content: string,
+  image?: string,
+  numberOfComments?: number,
+  numberOfRetweets?: number,
+  numberOfLikes?: number
+}
