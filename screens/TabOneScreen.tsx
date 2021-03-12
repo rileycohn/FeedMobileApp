2f14bb42ae83b1e611ceb0bc6f7d7b64d5e6936a
Ryan <rgerner@asu.edu>
2021-03-11 16:58:51 -0800

    Added a component for the feed display on the profile page. Added it statically to the example profile page.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { StyleSheet, Image } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import FeedDisp from '../components/FeedDisplay';
 
 export default function TabOneScreen() {
     return (
         <View style={styles.container}>
-            <View style={{ flex: 2, flexDirection: 'row' }}>
+            <View style={{ flex: 3, flexDirection: 'row' }}>
                 <Image source={require('../assets/images/profilePictureExample.jpg')} style={{ flex: 3, borderColor: 'gainsboro', borderWidth:10 }} />
                 <View style={{ flex: 5, justifyContent: 'center', backgroundColor:'gainsboro' }}>
                     <Text style={styles.pageText}>UserName123</Text>
@@ -14,17 +15,20 @@ export default function TabOneScreen() {
                 <View style={{ flex: 3, justifyContent: 'center', backgroundColor: 'gainsboro' }}>
                     <Text style={styles.pageText}>Posts:10</Text>
                 </View>
-                <View style={{ flex: 3, justifyContent: 'center', backgroundColor: 'gainsboro' }}>
+                <View style={{ flex: 3, justifyContent: 'center', backgroundColor: 'gainsboro', paddingRight:10 }}>
                     <Text style={styles.pageText}>Likes:500</Text>
                 </View>
             </View>
             <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'gainsboro' }}>
                 <Text style={styles.pageText}>I am a generic user on the app 'Feed'</Text>
             </View>
-            <View style={{ flex: 4, backgroundColor: 'gainsboro'}}>
+            <View style={{ flex: 7, backgroundColor: 'gainsboro'}}>
                 <Text style={styles.pageTextLeft}>Feeds:</Text>
+                <FeedDisp feedName='Feed1' followers='57' />
+                <FeedDisp feedName='Feed2' followers='113' />
+                <FeedDisp feedName='Feed3' followers='62' />
             </View>
-            <View style={{ flex: 4, backgroundColor: 'gainsboro'}}>
+            <View style={{ flex: 7, backgroundColor: 'gainsboro'}}>
                 <Text style={styles.pageTextLeft}>Recent Posts:</Text>
             </View>
     </View>
